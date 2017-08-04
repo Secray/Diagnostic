628ac06dc0a8e47632d3c357ac5d02fce6a503c3
xiekui <xiekui@wingtech.com>
2017-08-04 10:30:29 +0800

    update res by xiekui

--- app/src/main/java/com/wingtech/diagnostic/activity/BaseActivity.java
+++ app/src/main/java/com/wingtech/diagnostic/activity/BaseActivity.java
@@ -1,6 +1,5 @@
 package com.wingtech.diagnostic.activity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
